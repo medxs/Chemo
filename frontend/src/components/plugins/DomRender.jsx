5e6075e7dcd123b3838289140445ef6b201e2066
medxs <a.medasmardona@gmail.com>
2024-08-31 11:19:21 +0530

    remove all files

--- frontend/src/components/plugins/DomRender.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom";
-
-const DomRender = ({ children }) => {
-  return ReactDOM.createPortal(children, document.body);
-};
-
-export default DomRender;
--- frontend/src/helpers/getQueryParams.js
+++
@@ -1,5 +0,0 @@
-export const getQueryParam = (paramName) => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    return urlParams.get(paramName);
-}
