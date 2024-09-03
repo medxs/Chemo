204b8fc1a2ec4fa834246fdd5542fad6cc4840f2
medxs <a.medasmardona@gmail.com>
2024-09-03 08:13:26 +0530

    test 2

--- backend/utils/envHelper.js
+++ backend/utils/envHelper.js
@@ -1,8 +1,8 @@
 const getEnvironmentUrl = (req, res) => {
     const env = process.env.REACT_APP_NODE_ENV || 'development';
-    
+
     if (env === 'production') {
-        return `${req.protocol}://${req.get('host')}`;
+        return `http://44.208.21.217:5000`;
     } else {
         return process.env.BACKEND_URL || 'http://localhost:8000'; // Default fallback for development
     }
--- frontend/src/helpers/envHelper.js
+++ frontend/src/helpers/envHelper.js
@@ -1,7 +1,7 @@
 const getEnvironmentUrl = (req, res) => {
     const env = process.env.REACT_APP_NODE_ENV || 'development';
     if (env === 'production') {
-        return `${req.protocol}://${req.get('host')}`;
+        return `http://44.208.21.217:5000`;
     } else {
         return process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000'; // Default fallback for development
     }
